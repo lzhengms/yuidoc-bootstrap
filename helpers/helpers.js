ae4c898b806c9638e27d418bf1922ffb8a4619d3
crossjs <one@ffully.com>
2014-01-18 19:15:27 +0800

    update
    * remove yui urls in theme.json
    * read home content from project’s README.md
    *

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -4,64 +4,55 @@ path = require("path");
 markdown = require( "markdown" ).markdown;
 
 module.exports = {
-    publicClasses: function(context, options) {
-        'use strict';
-        var ret = "";
+  publicClasses: function(context, options) {
+    'use strict';
+    var ret = "";
 
-        for(var i=0; i < context.length; i++) {
-            if(!context[i].itemtype && context[i].access === 'public') {
-                ret = ret + options.fn(context[i]);
-            } else if (context[i].itemtype) {
-                ret = ret + options.fn(context[i]);
-            }
-        }
-
-        return ret;
-    },
-    search : function(classes, modules) {
-        'use strict';
-        var ret = '';
-
-        for(var i=0; i < classes.length; i++) {
-            if(i > 0) {
-                ret += ', ';
-            }
-            ret += "\"" + 'classes/' + classes[i].displayName + "\"";
-        }
-
-        if(ret.length > 0 && modules.length > 0) {
-            ret += ', ';
-        }
-
-        for(var j=0; j < modules.length; j++) {
-            if(j > 0) {
-                ret += ', ';
-            }
-            ret += "\"" + 'modules/' + modules[j].displayName + "\"";
-        }
-
-        return ret;
-    },
-    homeContent : function () {
-        var config = path.join(process.cwd(), 'yuidoc');
-        if (fs.existsSync(config + '.json') === true) {
-            config = require(config);
-        }
-
-        var content = config.theme.home
-        if (typeof content === 'string' && content !== '') {
-            content = path.resolve(process.cwd(), content);
-        } else {
-            content = path.resolve(process.cwd(), 'README.md');
-        }
-       
-        if (fs.existsSync(content) === true) {  
-            content = fs.readFileSync(content, {encoding: "utf8"});
-            content = markdown.toHTML(content);
-            content = "<div class=\"well\">" + content + "</div>"
-        } else {
-            content = '';
-        }
-        return content;
+    for(var i=0; i < context.length; i++) {
+      if(!context[i].itemtype && context[i].access === 'public') {
+        ret = ret + options.fn(context[i]);
+      } else if (context[i].itemtype) {
+        ret = ret + options.fn(context[i]);
+      }
     }
+
+    return ret;
+  },
+  search : function(classes, modules) {
+    'use strict';
+    var ret = '';
+
+    for(var i=0; i < classes.length; i++) {
+      if(i > 0) {
+        ret += ', ';
+      }
+      ret += "\"" + 'classes/' + classes[i].displayName + "\"";
+    }
+
+    if(ret.length > 0 && modules.length > 0) {
+      ret += ', ';
+    }
+
+    for(var j=0; j < modules.length; j++) {
+      if(j > 0) {
+        ret += ', ';
+      }
+      ret += "\"" + 'modules/' + modules[j].displayName + "\"";
+    }
+
+    return ret;
+  },
+
+  readme : function () {
+    var content = path.resolve(process.cwd(), 'README.md');
+
+    if (fs.existsSync(content) === true) {
+      content = fs.readFileSync(content, {encoding: "utf8"});
+      content = markdown.toHTML(content);
+    } else {
+      content = '';
+    }
+
+    return content;
+  }
 };
